2eddffb9cdfff3e89a7a60968953e32b3c66e8ed
pillypenkosa <pillypenkosa@gmail.com>
2024-01-29 10:07:51 +0200

    Films 2024-01-29 10:05

--- list/list.films.js
+++ list/list.films.js
@@ -1810,32 +1810,31 @@ const arrFilms = [
 	internet: { imdb: '0325703', wiki_ua: '%D0%9B%D0%B0%D1%80%D0%B0_%D0%9A%D1%80%D0%BE%D1%84%D1%82_%D1%80%D0%BE%D0%B7%D0%BA%D1%80%D0%B0%D0%B4%D0%B0%D1%87%D0%BA%D0%B0_%D0%B3%D1%80%D0%BE%D0%B1%D0%BD%D0%B8%D1%86%D1%8C:_%D0%BA%D0%BE%D0%BB%D0%B8%D1%81%D0%BA%D0%B0_%D0%B6%D0%B8%D1%82%D1%82%D1%8F', },
 	cast: { angelina_jolie_04061975: 1, gerard_butler_13111969: 1, },
 },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 {
 	id: 'into_the_blue_2005', title: { ua: 'Ласкаво просимо до раю', en: 'Into the Blue', }, year: 2005, country: { usa: 1, },
 	genre: { action: 1, adventure: 1, crime: 1, }, hash: { beach: 1, },
 	internet: { imdb: '0378109', wiki_ua: '%D0%9B%D0%B0%D1%81%D0%BA%D0%B0%D0%B2%D0%BE_%D0%BF%D1%80%D0%BE%D1%81%D0%B8%D0%BC%D0%BE_%D0%B4%D0%BE_%D1%80%D0%B0%D1%8E!_(%D1%84%D1%96%D0%BB%D1%8C%D0%BC,_2005)', ashdivip: '14357', },
 	cast: { paul_walker_12091973: 1, jessica_alba_28041981: 1, },
 },
+{
+	id: 'the_legend_of_Zorro_2005', title: { ua: 'Легенда Зорро', en: 'The Legend of Zorro', }, year: 2005, country: { usa: 1, },
+	genre: { action: 1, adventure: 1, },
+	internet: { imdb: '0386140', wiki_ua: '%D0%9B%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0_%D0%97%D0%BE%D1%80%D1%80%D0%BE', },
+	cast: { antonio_banderas_10081960: 1, catherine_zeta_jones_25091969: 1, },
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
 {
 	id: 'Leon_1994', title: { ua: 'Леон', en: 'Léon', }, year: 1994, country: { fra: 1, usa: 1, },
 	genre: { action: 1, crime: 1, drama: 1, }, hash: { killer: 1, love: 1, drugs: 1, children: 1, },
@@ -2206,6 +2205,12 @@ const arrFilms = [
 	genre: { action: 1, crime: 1, }, hash: { fight: 1, prison: 1, },
 	internet: { imdb: '1156466', wiki_ua: '%D0%9D%D0%B5%D0%B7%D0%B0%D0%BF%D0%B5%D1%80%D0%B5%D1%87%D0%BD%D0%B8%D0%B9_3', },
 	cast: { scott_adkins_17061976: 1, },
+},
+{
+	id: 'scarface_1983', title: { ua: 'Обличчя зі шрамом', en: 'Scarface', }, year: 1983, country: { usa: 1, },
+	genre: { drama: 1, crime: 1, },
+	internet: { imdb: '0086250', wiki_ua: '%D0%9E%D0%B1%D0%BB%D0%B8%D1%87%D1%87%D1%8F_%D0%B7%D1%96_%D1%88%D1%80%D0%B0%D0%BC%D0%BE%D0%BC_(%D1%84%D1%96%D0%BB%D1%8C%D0%BC,_1983)', },
+	cast: { al_pacino_25041940: 1, michelle_pfeiffer_29041958: 1, },
 },
 {
 	id: 'Ocean_s_eleven_2001', title: { ua: 'Одинадцять друзів Оушена', en: 'Ocean\'s Eleven', }, year: 2001, country: { usa: 1, },
@@ -3436,22 +3441,12 @@ const arrFilms = [
 
 
 
-{
-	id: 'legenda_Zorro_2005',
-	title: { ua: 'Легенда Зорро', en: 'The Legend of Zorro', },
-	year: 2005,
-	country: { usa: 1, },
-	internet: { imdb: '0386140', wiki_ua: '%D0%9B%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0_%D0%97%D0%BE%D1%80%D1%80%D0%BE', },
-	cast: { antonio_banderas_10081960: 1, catherine_zeta_jones_25091969: 1, },
-},
-{
-	id: 'lico_so_shramom_1983',
-	title: { ua: 'Обличчя зі шрамом', en: 'Scarface', },
-	year: 1983,
-	country: { usa: 1, },
-	internet: { imdb: '0086250', wiki_ua: '%D0%9E%D0%B1%D0%BB%D0%B8%D1%87%D1%87%D1%8F_%D0%B7%D1%96_%D1%88%D1%80%D0%B0%D0%BC%D0%BE%D0%BC_(%D1%84%D1%96%D0%BB%D1%8C%D0%BC,_1983)', },
-	cast: { al_pacino_25041940: 1, michelle_pfeiffer_29041958: 1, },
-},
+
+
+
+
+
+
 {
 	id: 'lovushka_dlya_koshek_1986',
 	title: { ua: 'Пастка для котів', en: 'Macskafogo', },
