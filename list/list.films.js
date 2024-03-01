ca1270ed13516a6d435b003a60ea475e5e95143b
pillypenkosa <pillypenkosa@gmail.com>
2024-03-01 11:07:38 +0200

    Films 2024-03-01 11:05

--- list/list.films.js
+++ list/list.films.js
@@ -607,6 +607,12 @@ const arrFilms = [
 	internet: { imdb: '6742252', wiki_ru: '%D0%92%D0%B8%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B9_(%D1%84%D0%B8%D0%BB%D1%8C%D0%BC,_2018)', },
 },
 {
+	id: 'le_grand_blond_avec_une_chaussure_noire_1972', title: { ua: 'Високий блондин у чорному черевику', en: 'Le grand blond avec une chaussure noire', }, year: 1972, country: { fra: 1, },
+	genre: { comedy: 1, },
+	internet: { imdb: '0068655', wiki_ua: '%D0%92%D0%B8%D1%81%D0%BE%D0%BA%D0%B8%D0%B9_%D0%B1%D0%BB%D0%BE%D0%BD%D0%B4%D0%B8%D0%BD_%D1%83_%D1%87%D0%BE%D1%80%D0%BD%D0%BE%D0%BC%D1%83_%D1%87%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%D0%BA%D1%83', },
+	cast: { pierre_richard_16081934: 1, },
+},
+{
 	id: 'enter_the_dragon_1973', title: { ua: 'Вихід Дракона', en: 'Enter the Dragon', }, year: 1973, country: { usa: 1, },
 	genre: { action: 1, }, hash: { fight: 1, },
 	internet: { imdb: '0070034', wiki_ua: '%D0%92%D0%B8%D1%85%D1%96%D0%B4_%D0%94%D1%80%D0%B0%D0%BA%D0%BE%D0%BD%D0%B0', },
@@ -2903,6 +2909,12 @@ const arrFilms = [
 	cast: { daniel_craig_02031968: 1, judi_dench_09121934: 1, },
 },
 {
+	id: 'la_chevre_1981', title: { ua: 'Невдахи', en: 'La chèvre', }, year: 1981, country: { fra: 1, mex: 1, fmlt: 1, },
+	genre: { comedy: 1, crime: 1, },
+	internet: { imdb: '0082183', wiki_ua: '%D0%9D%D0%B5%D0%B2%D0%B4%D0%B0%D1%85%D0%B8_(%D1%84%D1%96%D0%BB%D1%8C%D0%BC,_1981)', },
+	cast: { pierre_richard_16081934: 1, gerard_depardieu_27121948: 1, },
+},
+{
 	id: 'pure_luck_1991', title: { ua: 'Невдахи', en: 'Pure Luck', }, year: 1991, country: { usa: 1, },
 	genre: { comedy: 1, crime: 1, },
 	internet: { imdb: '0102729', wiki_ua: '%D0%9D%D0%B5%D0%B2%D0%B4%D0%B0%D1%85%D0%B8_(%D1%84%D1%96%D0%BB%D1%8C%D0%BC,_1991)', },
@@ -4759,6 +4771,12 @@ const arrFilms = [
 	genre: { action: 1, sci_fi: 1, crime: 1, }, hash: { time: 1, future: 1, },
 	internet: { imdb: '0104299', wiki_ua: '%D0%9A%D0%BE%D1%80%D0%BF%D0%BE%D1%80%D0%B0%D1%86%D1%96%D1%8F_%C2%AB%D0%91%D0%B5%D0%B7%D1%81%D0%BC%D0%B5%D1%80%D1%82%D1%8F%C2%BB_(%D1%84%D1%96%D0%BB%D1%8C%D0%BC)', },
 	cast: { emilio_estevez_12051962: 1, mick_jagger_26071943: 1, rene_russo_17021954: 1, anthony_hopkins_31121937: 1, },
+},
+{
+	id: 'les_fugitifs_1986', title: { ua: 'Утікачі', en: 'Les fugitifs', }, year: 1986, country: { fra: 1, },
+	genre: { comedy: 1, crime: 1, },
+	internet: { imdb: '0091093', wiki_ua: '%D0%92%D1%82%D1%96%D0%BA%D0%B0%D1%87%D1%96_(%D1%84%D1%96%D0%BB%D1%8C%D0%BC)', },
+	cast: { pierre_richard_16081934: 1, gerard_depardieu_27121948: 1, },
 },
 {
 	id: 'bad_teacher_2011', title: { ua: 'Училка', en: 'Bad Teacher', }, year: 2011, country: { usa: 1, },
