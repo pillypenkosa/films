202a7786d489e8180ce43574fe61cb01a9ab2a54
pillypenkosa <pillypenkosa@gmail.com>
2024-09-19 16:52:16 +0300

    Films 2024-09-19 16:10

--- list/list.franchise.js
+++ list/list.franchise.js
@@ -13,6 +13,10 @@ const objFilmFranchise = {
 
 back_to_the_future 		: [ 'back_to_the_future_1985', 'back_to_the_future_2_1989', 'back_to_the_future_3_1990', ],
 Beverly_Hills_cop 		: [ 'Beverly_Hills_cop_1984', 'Beverly_Hills_cop_2_1987', 'Beverly_Hills_cop_3_1994', 'Beverly_Hills_cop_4_2024', ],
+
+
+fast_furious 			: [ 'fast_&_furious_2001', 'fast_&_furious_2_2003', 'fast_&_furious_3_2006', 'fast_&_furious_4_2009', 'fast_&_furious_5_2011', 'fast_&_furious_6_2013', 'fast_&_furious_7_2015', 'fast_&_furious_8_2017', 'fast_&_furious_9_2021', 'fast_&_furious_10_2023', ],
+
 Rambo 					: [ 'Rambo_1982', 'Rambo_2_1985', 'Rambo_3_1988', 'Rambo_4_2008', 'Rambo_5_2019', ],
 robocop					: [ 'robocop_1987', 'robocop_2_1990', 'robocop_3_1993', 'robocop_2014', ],
 Rocky 					: [ 'Rocky_1976', 'Rocky_2_1979', 'Rocky_3_1982', 'Rocky_4_1985', 'Rocky_5_1990', 'Rocky_6_2006', ],
