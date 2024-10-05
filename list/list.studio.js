9e671b0fed4e72bfda1b19fce40b7c7d9a6e2759
pillypenkosa <pillypenkosa@gmail.com>
2024-10-05 22:53:14 +0300

    Films 2024-10-05 22:05

--- list/list.studio.js
+++ list/list.studio.js
@@ -4,15 +4,29 @@
 
 const arrFilmStudio = [
 
-	{ id: 'columbia_pictures' 			, title: 'Columbia Pictures' 		, },
-	{ id: 'dream_works' 				, title: 'DreamWorks' 				, },
-	{ id: 'hasbro' 						, title: 'Hasbro' 					, },
-	{ id: 'mgm' 						, title: 'Metro-Goldwyn-Mayer' 		, },
-	{ id: 'paramount_pictures' 			, title: 'Paramount Pictures' 		, },
-	{ id: 'universal_pictures' 			, title: 'Universal Pictures' 		, },
-	{ id: 'walt_disney' 				, title: 'Walt Disney' 				, },
-	{ id: 'warner_bros' 				, title: 'Warner Bros.' 			, },
-
+	{ id: '_20_century' 		, title: '20th Century' 			, },
+	{ id: 'columbia' 			, title: 'Columbia Pictures' 		, },
+	{ id: 'dimension' 			, title: 'Dimension Films' 			, },
+	{ id: 'dream_works' 		, title: 'DreamWorks' 				, },
+	{ id: 'hasbro' 				, title: 'Hasbro' 					, },
+	{ id: 'hollywood' 			, title: 'Hollywood Pictures' 		, },
+	{ id: 'lionsgate' 			, title: 'Lionsgate' 				, },
+	{ id: 'mgm' 				, title: 'Metro-Goldwyn-Mayer' 		, },
+	{ id: 'millennium' 			, title: 'Millennium Films' 		, },
+	{ id: 'miramax' 			, title: 'Miramax Films' 			, },
+	{ id: 'netflix' 			, title: 'Netflix' 					, },
+	{ id: 'new_line' 			, title: 'New Line Cinema' 			, },
+	{ id: 'original' 			, title: 'Original Film' 			, },
+	{ id: 'paramount' 			, title: 'Paramount Pictures' 		, },
+	{ id: 'pixar' 				, title: 'Pixar' 					, },
+	{ id: 'sony' 				, title: 'Sony Pictures' 			, },
+	{ id: 'stx' 				, title: 'STX Entertainment ' 		, },
+	{ id: 'asylum' 				, title: 'The Asylum' 				, },
+	{ id: 'touchstone' 			, title: 'Touchstone Pictures' 		, },
+	{ id: 'tristar' 			, title: 'TriStar Pictures' 		, },
+	{ id: 'universal' 			, title: 'Universal Pictures' 		, },
+	{ id: 'walt_disney' 		, title: 'Walt Disney' 				, },
+	{ id: 'warner' 				, title: 'Warner Bros.' 			, },
 
 
 
